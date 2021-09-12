a1253a072e1099ab6bbf28b1711d7d5771accfcb
Kaba <kaba.hansen@hotmail.com>
2021-09-12 11:44:28 +0200

    some glide
    can see it better with bigger force. press play to see w 50 force

--- Assets/Verletintegration.cs
+++ Assets/Verletintegration.cs
@@ -7,6 +7,7 @@ public class Verletintegration : MonoBehaviour
     [SerializeField] private Vector3 startVelocity;
     [SerializeField] private float mass = 1;
     [SerializeField] private float damping = 0.01f;
+    [SerializeField] private float floorDamping = 1.5f;
 
     public float Mass => mass;
 
@@ -16,6 +17,7 @@ public class Verletintegration : MonoBehaviour
     private Vector3 accumulatedForces = Vector3.zero;
     private float delta;
     private float inverseMass;
+    private float currentDamping = 0f;
 
     void Start()
     {
@@ -23,6 +25,7 @@ public class Verletintegration : MonoBehaviour
         currentPos = transform.position;
         previousPos = currentPos - startVelocity * delta;
         inverseMass = 1 / mass;
+        currentDamping = startVelocity.x * delta;
     }
 
     void FixedUpdate()
@@ -31,7 +34,27 @@ public class Verletintegration : MonoBehaviour
         if (GetComponent<collision>().GetIfFalling())
         {
             currentPos = (2 - damping) * currentPos - (1-damping)*previousPos + accumulatedForces * (delta * delta * inverseMass);
-        } // else: glide/bounce along floor
+        } // else: glide along floor
+        else
+        {
+            if(currentPos.x < previousPos.x)
+            {
+                currentPos = new Vector3(transform.position.x - Mathf.Abs(currentDamping), transform.position.y, transform.position.z);
+            }
+            else
+            {
+                currentPos = new Vector3(transform.position.x + currentDamping, transform.position.y, transform.position.z);
+            }
+            if(currentDamping > 0)
+            {
+                currentDamping = currentDamping / 2;
+            }
+            else
+            {
+                currentDamping = 0;
+            }
+            Debug.Log(currentDamping);
+        }
         previousPos = tempPos;
         transform.position = currentPos;
         accumulatedForces = Vector3.zero;
